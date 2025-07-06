aa5ed0af9f128fe756a1351282de0e7044071fbd
Daniel Sarpaneva <daniel.sarpaneva@gmail.com>
2025-07-06 18:08:07 +0300

    Sitemap ja muut

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://sarpanevawebsites.com', // Vaihda omaan domainiisi!
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: 'monthly',
+    priority: 0.7,
+  };
+  
