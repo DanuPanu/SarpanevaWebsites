d08567503b4ec7eca187ddc5a58542bcfe878661
Daniel Sarpaneva <daniel.sarpaneva@gmail.com>
2025-07-14 17:11:13 +0300

    New sitemap

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://sarpanevawebsites.com', 
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    changefreq: 'monthly',
-    priority: 0.7,
-  };
-  
